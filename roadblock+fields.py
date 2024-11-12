313ff0a42dbad585ffa73f00239316d66691c740
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-11-12 13:44:10 +0000

    Create roadblock+fields.py

---
+++ roadblock+fields.py
@@ -0,0 +1,13 @@
+def road_block(family):
+    print("You've leave your house and jump in the car.")
+    if family:
+        print("Your daughters are in the back and your wife is next to you in the passenger seat. Everyone is worriedly silent, not sure what to say to make the mood lighter.")
+    else:
+        print("The car is eerily silent until you see some form of civilization.")
+
+    print("There's a road block ahead. People are running everywhere and there's no chance of getting a car through this chaos.")
+    print("You decide to get out of the car and walk. You're not sure where you're going, but you know you have to get out of here.")
+    print("Before getting out the car, you try to formulate a plan.")
+
+def fields(family):
+    pass
