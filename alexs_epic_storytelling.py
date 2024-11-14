722c1d5abb2f1da33d87d070cec15c0a2f3332a5
FreedomwarEnjoyer <144335638+FreedomwarEnjoyer@users.noreply.github.com>
2024-11-14 09:50:23 +0000

    blahblagblah

--- alexs_epic_storytelling.py
+++ alexs_epic_storytelling.py
@@ -1,16 +1,25 @@
+import os
+
 def mil_base_outskirts():
-    ans = input("After a long walk along the heedgerows of the fields, you find yourself at a barbed wire fence with a sign reading \"NO ENTRY\". This must be the military base. You follow the fence west, as that must be where the entrance is.\n\nIn the distance, you can see the gate. Something is off. There is not a single person in sight. You get closer and realize that the gate has been forced open. Upon closer inspection, you notice bodies in and around the cars lined up in front of the gate. \nA firefight seems to have broken out between the military and an unknown third party, resulting in the death of a guard sitting in a gatehouse on the right side of the gate.\n\nSearch his body?\n\nY- Yes\nN- No\n\n")
 
-    if ans == "y":
-        os.system("cls")
-        print("A fowl stench attacks your nose. On his body, you obtain a keycard; but also another question: How is the guard's body emitting this stench if he's only died recently?\nYou brush it off and move on. As you leave, you feel like you saw something move in the corner of your eye.")
-        #he obtains the keycard here
-    else:
-        os.system("cls")
-        print("Even as a top of the line biologist, dead bodies give you the ick. You decide to pass.")
+    x = True
+    print("After a long walk along the heedgerows of the fields, you find yourself at a barbed wire fence with a sign reading \"NO ENTRY\". This must be the military base. You follow the fence west, as that must be where the entrance is.\n\nIn the distance, you can see the gate. Something is off. There is not a single person in sight. You get closer and realize that the gate has been forced open. Upon closer inspection, you notice bodies in and around the cars lined up in front of the gate. \nA firefight seems to have broken out between the military and an unknown third party, resulting in the death of a guard sitting in a gatehouse on the right side of the gate.")
 
-    print("You walk through yet another open field, towards a group of buildings.  One of them reads: \"MUNITIONS STORAGE\"")
+    while x:
+        ans = input("\n\nSearch his body?\n\nSearch his body.\nContinue forward and ignore it.\n\n")
+        if ans == "Search his body.":
+            print("A fowl stench assaults your nose. From his body, you obtain a keycard. Something bugs you, however: The guard has only died recently. How has his body rotten this badly?\nYou brush it off and move on. As you leave, you feel like you saw the body twitch.")
+            #he obtains the keycard here
+            x = False
+        elif ans == "Continue forward and ignore it.":
+            print("Even as a top of the line biologist, dead bodies give you the ick. You decide to pass.")
+            x = False
+        else:
+            print("Invalid answer. Try again.")
+            pass
+
+    print("\nYou walk through yet another open field, towards a group of buildings. While walking, you hear distant gunshots. Whoever was fighting at the gate has clearly made it inside.\nYou reach a statue infront of a building. The plaque on the building reads: \"MUNITIONS STORAGE\".\nSuddenly, an explosion causes you to stagger and hide behind the statue. A hole is blown through a hallway connecting the munitions storage to another building. You see silhouettes of soldiers firing and advancing through the smoke.\n\nA lightbulb fires up in your head. That hole would be the perfect way to enter the building, and hopefully find a bunker of sorts, with enough supplies to last the apocalypse. It is a military base, after all.")
+    print("You make a run for the hole in the wall. Once inside, you take a left and run down hallways upon hallways filled with debris and smoke. Eventually, you find a flight of stairs that goes down. The smoke and debris has disappeared.\n\nWhile descending, a booming voice on the intercom invades your ears:\n\"ATTENTION ALL PERSONNEL, AN AIR STRIKE HAS BEEN ORDERED ON THIS BASE IN T-60 MINUTES. EVACTUATION HELICOPTERS HAVE BEEN CALLED TO SECTOR 4. IF YOU DO NOT EVACUATE, YOU WILL BE LEFT BEHIND.\nI REPEAT, AN AIR STRIKE HAS BEEN ORDERED ON THIS BASE IN T-60 MINUTES. EVACTUATION HELICOPTERS HAVE BEEN CALLED TO SECTOR 4. IF YOU DO NOT EVACUATE, YOU WILL BE LEFT BEHIND.\"\n\nYour heart sinks. You need to find this bunker, and fast.\n\nYou make your way down 10 or more flight of stairs, now at the bottom.\n\nYou see 6 or so doors. All but 1 are locked. Opening it reveales another hallway, with 2 metal pocket doors at the end. They seem to be powered by hydraulics.\n\nOne of them is slightly ajar, while the other one is shut.")
 
     
-def mil_base():
-    pass
+mil_base_outskirts()
