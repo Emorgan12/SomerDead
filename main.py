d1adbdfb7fc487e2efc3b7d8b750ab368a8a6652
Elliot <140021772+Emorgan12@users.noreply.github.com>
2024-12-13 14:29:41 +0000

    remove unnecsary sleeps, all remaining to be replaced

--- main.py
+++ main.py
@@ -301,31 +301,22 @@ def house():
 def road_block(family):
     system('clear')
     print("You leave your house and jump in the car.")
-    sleep(2)
     if family:
         print("Your daughters are in the back and your wife is next to you in the passenger seat. Everyone is worriedly silent, not sure what to say to make the mood lighter.")
     else:
         print("The car is eerily silent until you see some form of civilization.")
-    sleep(2)
     print("There's a road block ahead. People are running everywhere and there's no chance of getting a car through this cha")
-    sleep(2)
     print("You can see a handful of the strange-looking creatures that caused all this. They're definitely dangerous")
-    sleep(2)
     print("You conclude that there's no option but to get out of the car and walk. You're not sure where you're going, but you know you have to get out of here.")
-    sleep(2)
     print("Before getting out the car, you try to formulate a plan.")
     sleep(2)
     system('clear')
     print("There's a military base nearby, you could try to get there.")
-    sleep(2)
     print("It won't be easy, but it's the best option for safety.")
-    sleep(2)
     system('clear')
     if family:
         print("You continue, your wife holding Chelsea's hand and you holding Amber's.")
-        sleep(2)
         print("Above everything, you need to keep your family safe.")
-        sleep(2)
         print("Especially Amber and Chelsea, they're so young and innocent.")
     else:
         print("You continue, alone, worry for your family in the back of your mind")
@@ -336,13 +327,9 @@ def road_block(family):
 
 
 def fields(family):
-    system('clear')
     print("You've not been walking long, maybe 15 minutes, when you come across a field.")
-    sleep(2)
     print("The field is bright green with specs of other colours. It's serene and calm.")
-    sleep(1)
     print("There's no one in the field.")
-    sleep(2)
     if family:
         print("Just you, and your family.")
     else:
@@ -350,20 +337,14 @@ def fields(family):
     sleep(2)
     system('clear')
     print("There's a few birds, but they're dead silent.")
-    sleep(2)
     print("It's as if they're aware and of the danger and they're scared.")
-    sleep(2)
     system('clear')
     if family:
         print("Despite the fact you've not been walking long, Chelsea and Amber are complaining their legs hurt.")
-        sleep(2)
         print("You decide to take a break, and sit down in the field, but not for long.")
-        sleep(2)
         system('clear')
         print("You're sat there for around 2 minutes, quiet talking in an attempt to calm them down.")
-        sleep(2)
         print("You're scared, but you know everything will be okay when you get to the military base, together")
-        sleep(2)
         print("You need to keep moving, you all stand up and continue walking.")
     else:
         print("The field is empty, most people are running in the roads and in the streets, aimlessly")
@@ -371,34 +352,25 @@ def fields(family):
     sleep(2)
     system('clear')
     print("The fields feel safe, but you must keep your guard up, nowhere is safe when there's these creatures flooding the streets")
-    sleep(2)
     print("Anything could appear, you have to stay aware of your surroundings")
     sleep(2)
     system('clear')
     print("You come accross a massive patch of mud.")
-    sleep(2)
     print("There's a path that goes around, but that could add another 15-20 minutes to your journey")
-    sleep(1)
     print("You have to decide whether to go around it or go through it.")
-    sleep(2)
     if family:
         print("Both Chelsea and Amber are urging you to go around,")
-        sleep(1)
         print("but your wife trusts your decision will be best.")
         sleep(2)
     system('clear')
     while True:
         print("Will you go through the mud?")
-        sleep(0.5)
         ans = input("Y- Yes \nN- No \n\n").upper()
         if ans == "Y":
             system('clear')
             print("You go through the mud")
-            sleep(2)
             print("It's deeper than you thought.")
-            sleep(2)
             print("Your entire feet and ankles are submerged.")
-            sleep(2)
             if family:
                 print("Not being very tall, it comes up halfway up the bottom of your daughter's legs")
                 sleep(2)
@@ -421,10 +393,8 @@ def fields(family):
             system('clear')
 
     print("You walk for a little while longer until something catches your eye, it's silver and shiny")
-    sleep(2)
     print("On closer inspection, you realise it's a crowbar. Do you take it?")
     while True:
-        sleep(0.5)
         print("1. Take the crowbar")
         print("2. Leave the crowbar \n")
         
@@ -439,11 +409,9 @@ def fields(family):
             inventory.append(1)
             break
         if ans == "2":
-            sleep(2)
             print("You leave the crowbar, it'll be too difficult to carry with you.")
             break
         else:
-            sleep(2)
             print("Invalid input")
     print("You continue walking for about 30 minutes until you spot your destination up ahead")
     return inventory
